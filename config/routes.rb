7c5af5b39216c2b205e41e90ada23997b46e4888
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-13 11:45:02 -0600

    Setup pages/dashboard controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get "/dashboard", to: "dashboard#index"
+  get "pages/home"
   resources :user_projects
   resources :projects
   resources :skills
   devise_for :users
+
+  root to: "pages#home"
+
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
 end
