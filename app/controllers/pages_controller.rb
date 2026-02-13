5b5d688d2bcf51ea624c46504e5af497af8f4880
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-13 13:41:15 -0600

    bugs, fixed functionality of /dashboard and/homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
-    # if user_signed_in?
-    #   redirect_to dashboard_url
-    # else
+    if user_signed_in?
+      redirect_to dashboard_url
+    else
       render :home
     end 
   end
