aed511701c93c40545613d2e79f27ed6c5111636
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-14 10:17:00 -0600

    Revert "big fixes"
    This reverts commit 54b2264e489e9e7ca85fdb472f2d210a40aaf7ae.

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
   resources :skills
   devise_for :users
 
-  get ":username" => "users#show", as: :user
-  get("/", { :controller => "pages", :action => "home" })
-  get("/index", { :controller => "pages", :action => "index" })
-  get("/dashboard", { :controller => "dashboard", :action => "index" })
+
+get("/", { :controller => "pages", :action => "home" })
+get("/index", { :controller => "pages", :action => "index" })
+get("/dashboard", { :controller => "dashboard", :action => "index" })
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
 end
