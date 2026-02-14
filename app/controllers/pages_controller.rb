c627101ace10424d13d6006bad82530603af5c79
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-14 08:36:21 -0600

    Set homepage to pages#home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def home
-    if user_signed_in?
-      redirect_to dashboard_url
-    else
+    # if user_signed_in?
+    #   redirect_to dashboard_url
+    # else
       render :home
     end
   end
-end
+# end
