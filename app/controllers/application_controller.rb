fc56c008a9b8f92f68839a1f8bdac9301f30bd50
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-16 10:50:17 -0600

    forgot to save before commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  skip_forgery_protection
   allow_browser versions: :modern
 
-  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:first_name])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:first_name])
+  end
 end
