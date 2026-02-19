62b58d4111f1599cedbb86361c3ec88e7f069de1
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-19 10:14:16 -0600

    profiles form view created from users existing model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, :keys => [:first_name])
-
-    devise_parameter_sanitizer.permit(:account_update, :keys => [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:first_name, :last_name, :bio, :skill_ids => []])
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:first_name, :last_name, :bio, :skill_ids => []])
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,58 +1,18 @@
 class ProfilesController < ApplicationController
-  before_action :set_profile, only: %i[ show edit update destroy ]
+  before_action :set_profile, only: %i[ show ]
 
   # GET /profiles
   def index
-    @profiles = Profile.all
+    @users = User.all
   end
 
   # GET /profiles/1
   def show
   end
 
-  # GET /profiles/new
-  def new
-    @profile = Profile.new
-  end
-
-  # GET /profiles/1/edit
-  def edit
-  end
-
-  # POST /profiles
-  def create
-    @profile = Profile.new(profile_params)
-
-    if @profile.save
-      redirect_to @profile, notice: "profile was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /profiles/1
-  def update
-    if @profile.update(profile_params)
-      redirect_to @profile, notice: "profile was successfully updated.", status: :see_other
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /profiles/1
-  def destroy
-    @profile.destroy!
-    redirect_to profiles_path, notice: "Profile was successfully destroyed.", status: :see_other
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_profile
-      @profile = profile.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def profile_params
-      params.expect(profile: [ :name ])
-    end
+  def set_profile
+    @user = User.find(params.expect(:id))
+  end
 end
